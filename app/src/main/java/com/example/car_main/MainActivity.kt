59443fcb0783715b121f55155f60bf93595ef94e
Dominik Bubenik <bubenik1@stud.uniza.sk>
2024-04-07 23:30:56 +0200

    create views Graphs, Stats, TimeLine

--- app/src/main/java/com/example/car_main/MainActivity.kt
+++ app/src/main/java/com/example/car_main/MainActivity.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.tiptime
+package com.example.car_main
 
 import android.app.Activity
 import android.app.AlertDialog
